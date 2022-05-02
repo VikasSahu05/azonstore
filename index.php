41627ffeec6853a25bfd849e8f53ec1da5f377ef
Vikas Kumar Sahu <vikassaranga002@gmail.com>
2022-05-02 18:42:38 +0530

    head

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php include("functions/functions.php");?>
 <!DOCTYPE html>
 <html lang="en">
--- view/header1.php
+++ view/header1.php
@@ -14,7 +14,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="../css/header1.css">
-    <script src="../js/header.js"></script>
+    <!-- <script src="../js/header.js"></script> -->
     
 </head>
 <body>
@@ -23,29 +23,76 @@
         <div class="container containerToHeader">
             <div class="row headerRow v-center">
                 <div class="topHeaderItem item-left">
-                    <div id="logo">
+                    <div id="logo" class="d-block d-lg-none">
                         <a href="#">Azon store</a>
                     </div>
                 </div>
                 <!-- menu start from here -->
                 <div class="topHeaderItem item-center">
+                    <div class="menu-overlay">
+
+                    </div>
                     <nav class="menu">
                         <div class="mobile-menu-head">
                             <div class="go-back">
                                 <i class="fa fa-angle-left"></i>
                             </div>
-                            <div class="current-menu-title">Shop</div>
+                            <div class="current-menu-title"></div>
                             <div class="mobile-menu-close">&times;</div>
                         </div>
                         <ul class="menu-main ulHeaderMain">
-                            <li class="navItems">
-                                <a href="#">Home</a>
+                        <li class="navItems menu-item-has-children">
+                                <a href="#">Fashion<i class="fa fa-angle-down"></i></a>
+                                <div class="sub-menu mega-menu mega-menu-col-4">
+                                    <div class="list_itemSub">
+                                        <h6 class="title">Fashion</h6>
+                                        <ul>
+                                            <li><a href="#">list</a></li>
+                                            <li><a href="#">list</a></li>
+                                            <li><a href="#">list</a></li>
+                                            <li><a href="#">list</a></li>
+                                            <li><a href="#">list</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="list_itemSub">
+                                        <h6 class="title">Women's Fashion</h6>
+                                        <ul>
+                                            <li><a href="#">list</a></li>
+                                            <li><a href="#">list</a></li>
+                                            <li><a href="#">list</a></li>
+                                            <li><a href="#">list</a></li>
+                                            <li><a href="#">list</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="list_itemSub">
+                                        <h6 class="title">Home , Kitchen</h6>
+                                        <ul>
+                                            <li><a href="#">list</a></li>
+                                            <li><a href="#">list</a></li>
+                                            <li><a href="#">list</a></li>
+                                            <li><a href="#">list</a></li>
+                                            <li><a href="#">list</a></li>
+                                        </ul>
+                                        <h6 class="title">Kids</h6>
+                                        <ul>
+                                            <li><a href="#">list</a></li>
+                                            <li><a href="#">list</a></li>
+                                            <li><a href="#">list</a></li>
+                                            <li><a href="#">list</a></li>
+                                            <li><a href="#">list</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="list_itemSub">
+                                        <img src="../img/imgg/tshir-tm.png" alt="">
+                                    </div> 
+                                </div>
+
                             </li>   
                             <li class="navItems">
                                 <a href="#">New</a>
                             </li>   
                             <li class="navItems menu-item-has-children">
-                                <a href="#">Shop <i class="fa fa-angle-down"></i></a>
+                                <a href="#">grocery <i class="fa fa-angle-down"></i></a>
                                 <div class="sub-menu single-column-Submenu">
                                     <ul>
                                         <li><a href="#">Mens</a></li>
@@ -103,19 +150,31 @@
 
                             </li>   
                             <li class="navItems">
-                                <a href="#">blog</a>
+                                <a href="#">Books</a>
                             </li>   
                             <li class="navItems">
-                                <a href="#">contact</a>
-                            </li>   
+                                <a href="#">Electronics</a>
+                            </li> 
+                            <li class="navItems">
+                                <a href="#">Household</a>
+                            </li>  
+                            <li class="navItems">
+                                <a href="#">Decoration</a>
+                            </li> 
+                            <li class="navItems">
+                                <a href="#">Furniture</a>
+                            </li>    
+                            <li class="navItems">
+                                <a href="#">Stationary</a>
+                            </li>    
                         </ul>
                     </nav>
                 </div>
                 <!-- menu end here -->
                 <div class="topHeaderItem item-right">
-                    <a href="#"><i class="fa fa-search"></i></a>
+                    <!-- <a href="#"><i class="fa fa-search"></i></a>
                     <a href="#"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
-                    <a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+                    <a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i></a> -->
                     <div class="mobile-menu-triggerTop">
                         <span></span>
                     </div>
@@ -129,5 +188,83 @@
 
     </section>
     <!-- banner end -->
+
+<!-- 
+    <script>
+        const mainMenuNav = (e) =>{
+            console.log(e.target.closest(".menu-item-has-children"));
+            // console.log(e,'hello');
+        }
+        mainMenuNav();
+    </script> -->
+
+
+    <script>
+        const menu = document.querySelector('.menu');
+        const mainMenu = menu.querySelector('.menu-main');
+        const goBack = menu.querySelector('.go-back');
+        const menuTrigger = document.querySelector('.mobile-menu-triggerTop');
+        const closeMenu = menu.querySelector('.mobile-menu-close');
+        let subMenu;
+        mainMenu.addEventListener("click",(e) =>{
+            if(!menu.classList.contains("Menuactive")){
+                return;
+            }
+           if(e.target.closest(".menu-item-has-children")){
+               const hasChildren = e.target.closest(".menu-item-has-children");
+               console.log(hasChildren);
+               showSubMenu(hasChildren);
+            }
+        });
+
+        goBack.addEventListener("click",()=>{
+            hideSubMenu();
+        });
+        menuTrigger.addEventListener("click",()=>{
+            toggleMenu();
+        });
+
+        closeMenu.addEventListener("click",()=>{
+            toggleMenu();
+        });
+
+        document.querySelector(".menu-overlay").addEventListener("click",()=>{
+            toggleMenu();
+        })
+
+        function toggleMenu(){
+            menu.classList.toggle("Menuactive");
+            document.querySelector(".menu-overlay").classList.toggle("Menuactive");
+        }
+        function showSubMenu(hasChildren){
+              subMenu = hasChildren.querySelector(".sub-menu");  
+              console.log(subMenu);
+              subMenu.classList.add("Menuactive")
+              subMenu.style.animation = "slideLeft 0.5s ease forwards"; 
+              const menuTitle = hasChildren.querySelector("i").parentNode.childNodes[0].textContent;
+              console.log(menuTitle);
+
+              menu.querySelector(".current-menu-title").innerHTML= menuTitle
+              menu.querySelector(".mobile-menu-head").classList.add("Menuactive")
+        }
+
+
+        function hideSubMenu(){
+            subMenu.style.animation = "slideRight 0.5s ease forwards"; 
+            setTimeout(()=>{
+                subMenu.classList.remove("Menuactive");
+            },300);
+              menu.querySelector(".current-menu-title").innerHTML="";
+              menu.querySelector(".mobile-menu-head").classList.remove("Menuactive")
+        }
+
+        window.onsize = function(){
+            if(this.innerWidth > 991){
+                if(menu.classList.contains("Menuactive")){
+                    toggleMenu();
+                }
+            }
+        }
+    </script>
 </body>
 </html>
